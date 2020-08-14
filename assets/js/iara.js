4ba6b2dd29623f00d40295e19760cc62e2cb9a02
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2020-08-14 22:22:35 +0200

    Cleanup

--- assets/js/iara.js
+++ assets/js/iara.js
@@ -2,48 +2,7 @@ $(document).ready(function(){
 
   function shareUrl(url, title, w, h) {
     window.open(url, "_blank").focus();
-    // var left = (screen.width / 2) - (w / 2);
-    // var top = (screen.height / 2) - (h / 2);
-    // var targetWin = window.open(
-    //   pageURL,
-    //   title,
-    //   "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,copyhistory=no,width="+w+",height="+h+",top="+top+",left="+left);
-
-    // return targetWin;
   }
-
-  // function executeGoogleSearch() {
-  //   var element = google.search.cse.element.getElement("searchresults-only0");
-  //   var search = $(".nav-search input[type=search]").val();
-
-  //   if (search) {
-  //     element.execute(search);
-  //   } else {
-  //     element.clearAllResults();
-  //   }
-  // }
-
-  function checkCarousel() {
-    var diff = $(".nav-scroller > nav")[0].scrollWidth - $('.nav-scroller')[0].scrollWidth
-
-    if (diff == 0) {
-      $(".nav-scroller-left, .nav-scroller-right").hide();
-    }
-  }
-
-  // $("#navbar").collapse({
-  //   toggle: false
-  // });
-
-  // $(".dropdown-toggle").dropdown();
-
-  // $(".search > a").click(function (e) {
-  //   e.preventDefault();
-  // });
-
-  // $(".search > a").focus(function() {
-  //   $(".search > input").focus();
-  // });
 
   $(".twitter-share-button").click(function (e) {
     e.preventDefault();
@@ -81,21 +40,6 @@ $(document).ready(function(){
     shareUrl(`https://pinterest.com/pin/create/button/?url=${url}&media=&description=${text}`, "Share on Pinterest", 600, 600);
   });
 
-  // $(".nav-search input[type=search]").keypress(function(event) {
-  //   if (event.which == 13) {
-  //     event.preventDefault();
-  //     executeGoogleSearch();
-  //   }
-  // });
-
-  // $(".nav-search input[type=search]").focusout(function() {
-  //   $(this).val("");
-  // });
-
-  // $(".nav-search .search-btn").click(function (e) {
-  //   e.preventDefault();
-  // });
-
   $(".nav-scroller-left").click(function (e) {
     e.preventDefault();
 
@@ -130,6 +74,18 @@ $(document).ready(function(){
       $(".nav-scroller-right").attr("disabled", "true");
     }
   });
+
+  function checkCarousel() {
+    if ($(".nav-scroller").length === 0) {
+      return;
+    }
+
+    var diff = $(".nav-scroller > nav")[0].scrollWidth - $('.nav-scroller')[0].scrollWidth
+
+    if (diff == 0) {
+      $(".nav-scroller-left, .nav-scroller-right").hide();
+    }
+  }
 
   checkCarousel();
 });
